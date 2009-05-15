81996a421b99890b3a46adb452932071b70b63e2
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2009-05-15 01:30:46 -0400

    Added set dotfiles script

---
+++ set_dotfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+FOLDER=`pwd`
+DIR=$HOME
+
+ln -fs $FOLDER/dir_colors $HOME/.dir_colors 
+ln -fs $FOLDER/gitconfig $HOME/.gitconfig 
+ln -fs $FOLDER/gvimrc $HOME/.gvimrc 
+ln -fs $FOLDER/htoprc $HOME/.htoprc 
+ln -fs $FOLDER/mrxvtrc $HOME/.mrxvtrc 
+ln -fs $FOLDER/screenrc $HOME/.screenrc 
+ln -fs $FOLDER/ssh_config $HOME/.ssh_config 
+ln -fs $FOLDER/vim $HOME/.vim 
+ln -fs $FOLDER/vimrc $HOME/.vimrc 
+ln -fs $FOLDER/wmiirc $HOME/.wmiirc 
+ln -fs $FOLDER/Xdefaults $HOME/.Xdefaults 
+ln -fs $FOLDER/Xmodmap $HOME/.Xmodmap 
+ln -fs $FOLDER/zshenv $HOME/.zshenv 
+ln -fs $FOLDER/zshrc $HOME/.zshrc
