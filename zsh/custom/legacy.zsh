040ccfb9eda96fc7adbcf85627f7d70a5635deff
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2012-07-28 17:49:17 -0400

    Comment out zkbd

--- zsh/custom/legacy.zsh
+++ zsh/custom/legacy.zsh
@@ -80,52 +80,52 @@ alias pacman='sudo pacman -y'
 
 #exec 2>>(while read line; do
   #print "${fg[red]}${(q)line}${reset_color}" > /dev/tty; print -n $'\0'; done)
-function zkbd_file() {
-    local zkbddir="$1"
-    local t1="$zkbddir/${TERM}-${VENDOR}-${OSTYPE}"
-    local t2="$zkbddir/${TERM}-${DISPLAY}"
-    if [[ -f $t1 ]]; then
-        printf '%s' "$t1"
-    elif [[ -f $t2 ]]; then
-        printf '%s' "$t2"
-    else
-        return 1
-    fi
-}
-
-function load_zkbd() {
-    autoload zkbd
-    local zkbddir=${ZDOTDIR:-$HOME}/.zkbd
-    [[ ! -d $zkbddir ]] && mkdir $zkbddir
-    local zkbdfile
-    zkbdfile=$(zkbd_file "$zkbddir")
-    local ret=$?
-    if [[ $ret -ne 0 ]]; then
-        zkbd
-        zkbdfile=$(zkbd_file "$zkbddir")
-        ret=$?
-    fi
-    if [[ $ret -eq 0 ]]; then
-        source "$zkbdfile"
-    else
-        echo 'Failed to setup keys using zkbd' 1>&2
-        return 1
-    fi
-
-    # setup key accordingly
-    [[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
-    [[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
-    [[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
-    [[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
-    [[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      up-line-or-history
-    [[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    down-line-or-history
-    [[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
-    #bindkey  ";5D"    backward-word
-    #bindkey  ";5C"   forward-word
-}
-load_zkbd || true
-unfunction load_zkbd
-unfunction zkbd_file
+#function zkbd_file() {
+#    local zkbddir="$1"
+#    local t1="$zkbddir/${TERM}-${VENDOR}-${OSTYPE}"
+#    local t2="$zkbddir/${TERM}-${DISPLAY}"
+#    if [[ -f $t1 ]]; then
+#        printf '%s' "$t1"
+#    elif [[ -f $t2 ]]; then
+#        printf '%s' "$t2"
+#    else
+#        return 1
+#    fi
+#}
+#
+#function load_zkbd() {
+#    autoload zkbd
+#    local zkbddir=${ZDOTDIR:-$HOME}/.zkbd
+#    [[ ! -d $zkbddir ]] && mkdir $zkbddir
+#    local zkbdfile
+#    zkbdfile=$(zkbd_file "$zkbddir")
+#    local ret=$?
+#    if [[ $ret -ne 0 ]]; then
+#        zkbd
+#        zkbdfile=$(zkbd_file "$zkbddir")
+#        ret=$?
+#    fi
+#    if [[ $ret -eq 0 ]]; then
+#        source "$zkbdfile"
+#    else
+#        echo 'Failed to setup keys using zkbd' 1>&2
+#        return 1
+#    fi
+#
+#    # setup key accordingly
+#    [[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
+#    [[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
+#    [[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
+#    [[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
+#    [[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      up-line-or-history
+#    [[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    down-line-or-history
+#    [[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
+#    #bindkey  ";5D"    backward-word
+#    #bindkey  ";5C"   forward-word
+#}
+#load_zkbd || true
+#unfunction load_zkbd
+#unfunction zkbd_file
 
 [[ -f /etc/zsh_command_not_found ]] && source /etc/zsh_command_not_found
 [[ -f ~/.dir_colors ]] && which dircolors >/dev/null 2>&1 && eval "`dircolors ~/.dir_colors -b`"
