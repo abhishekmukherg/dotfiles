445da4078284d846a671e2b5db19f8b3969724b6
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2012-08-25 14:11:07 -0400

    remove the stupid TRTOP thing

--- zsh/custom/legacy.zsh
+++ zsh/custom/legacy.zsh
@@ -24,6 +24,7 @@ setopt extended_history auto_pushd inc_append_history hist_ignore_dups
 setopt hist_verify auto_continue multios interactive_comments autocd
 setopt extended_glob notify list_ambiguous
 setopt noshare_history
+setopt noauto_name_dirs
 DIRSTACKSIZE=50
 limit coredumpsize 10m
 
