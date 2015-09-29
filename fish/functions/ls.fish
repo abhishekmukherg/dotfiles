9f4080a852743da9b1c5c852696129cfc08abb10
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2015-09-29 11:34:34 -0700

    add darwin support to ls

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,12 @@
 function ls
-    command ls --color=auto $argv
+	# Just calculate this once, to save a few cycles when displaying the prompt
+    if not set -q __fish_ls_uname
+        set -g __fish_ls_uname (uname)
+    end
+
+    if [ $__fish_ls_uname = 'Darwin' ]
+        command ls -G $argv
+    else
+        command ls --color=auto $argv
+    end
 end
