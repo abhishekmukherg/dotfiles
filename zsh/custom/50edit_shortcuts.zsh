943422e7903931059f54c2c2d8f76e8b8408d8e9
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2015-10-14 16:14:25 -0400

    update edit shortcuts more

--- zsh/custom/50edit_shortcuts.zsh
+++ zsh/custom/50edit_shortcuts.zsh
@@ -1,5 +1,5 @@
 function vz() {
-    (cd $zshhome; vim custom/ $HOME/.wzshrc ../zshenv)
+    (cd $zshhome; vim ../zshrc custom/ $HOME/.wzshrc ../zshenv)
 }
 function vv() {
     (cd $(dirname $(readlink ~/.vimrc)); vim $(basename $(readlink ~/.vimrc)))
