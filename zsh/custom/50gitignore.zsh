27ced03d9dd68b5bed3d4d83597080dc0a5cebd3
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2016-10-18 15:18:03 -0700

    remove gitignore

--- zsh/custom/50gitignore.zsh
+++
@@ -1,9 +0,0 @@
-## Gitignore.io
-function gi() {
-    local output="$(curl -s http://www.gitignore.io/api/$@)"
-    if [[ $1 == "list" ]]; then
-        echo "$output" | tr ',' '\n' | sort | column | column | column -t
-    else
-        echo "$output"
-    fi
-}
